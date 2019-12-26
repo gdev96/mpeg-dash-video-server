885bc6585ba810b56091743455e87c338e2a288c
giorgio <uni371032@studium.unict.it>
2019-12-26 16:32:52 +0100

    Edit docker-compose and add nginx locations

---
+++ src/main/java/com/unict/dieei/pr20/videomanagementservice/VideoManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.unict.dieei.pr20.videomanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoManagementServiceApplication.class, args);
+    }
+
+}
