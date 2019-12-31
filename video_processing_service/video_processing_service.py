7cc222795291913c51fd13f706cd23173ffa670b
Marco Mannino <ilmanno96@gmail.com>
2019-12-31 17:10:23 +0100

    add stats processing_service

--- video_processing_service/video_processing_service.py
+++ video_processing_service/video_processing_service.py
@@ -4,9 +4,16 @@ from flask import request
 import mysql.connector
 import os
 import subprocess
-
+import timeit
 
 app = Flask(__name__)
+
+cnx = mysql.connector.connect(
+        host="mpeg-dash-video-server_db_1",
+        user=os.environ['DB_USER'],
+        password=os.environ['DB_PASSWORD'],
+        database=os.environ['DB_NAME']
+    )
 
 
 @app.route("/")
@@ -21,25 +28,33 @@ def get_tasks():
 
 @app.route('/videos/process', methods=['POST'])
 def upload_video():
+    start = timeit.timeit()
+
+    # CALL SCRIPT
     video_id = request.json['videoId']
     cmd = "./encoder.sh " + str(video_id)
     subprocess.call(cmd, shell=True)
 
-    # INSERT VALUES IN DB
+    # INSERT STATISTICS IN DB
     my_cursor = cnx.cursor()
-    sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-    val = ("John", "Highway 21")
+    component_name = "Video Processing Service"
+    api = request.method + request.base_url
+    input_payload_size = request.content_length
+    x_request_id = request.headers.get("X-REQUEST-ID")
+    sql = "INSERT INTO call_stats (api, component_name, input_payload_size, output_payload_size, response_time, status_code, x_request_id) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    response = app.make_response('Video Encoded')
+    output_payload_size = response.content_length
+    status_code = response.status_code
+    stop = timeit.timeit()
+    response_time = stop - start
+    val = (api, component_name, input_payload_size, output_payload_size, response_time, status_code, x_request_id)
     my_cursor.execute(sql, val)
     cnx.commit()
-
-    return jsonify({'Response': 'Video Encoded'})
+    return response
 
 
 if __name__ == "__main__":
-    cnx = mysql.connector.connect(
-        host="mysql://db_1:3306",
-        user=os.environ['DB_USER'],
-        password=os.environ['DB_PASSWORD'],
-        database=os.environ['DB_NAME']
-    )
     app.run(host="0.0.0.0")
+
+
+
