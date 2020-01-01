31f1349e472318793d0ed1f41a3b80876e12f947
giorgio <uni371032@studium.unict.it>
2020-01-01 17:56:13 +0100

    Edit LogFilter order in Spring Security filter chain

--- video_management_service/src/main/java/com/unict/dieei/pr20/videomanagementservice/SecurityConfig.java
+++ video_management_service/src/main/java/com/unict/dieei/pr20/videomanagementservice/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .addFilterBefore(logFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(logFilter, ExceptionTranslationFilter.class)
                 .httpBasic()
                 .and()
                 .sessionManagement()
